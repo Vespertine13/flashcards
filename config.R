276d6a69efe3cdec155ed1a56d1b25998e67129b
Eivind Kjeka Broen <60471005+Vespertine13@users.noreply.github.com>
2023-06-21 15:01:31 +0200

    BASE FILES ADDED

---
+++ config.R
@@ -0,0 +1,6 @@
+# config file for flashcards
+
+setwd("../")
+path <- paste0(getwd(),"/mega/flashcards_data/spanish.csv")
+reverse = FALSE
+
---
+++ main.R
@@ -0,0 +1,31 @@
+# flash card function
+# functionality
+# take in a csv or parquet
+# give first a random word and then when prompted again give the "translation"
+source("config.R")
+
+data <- read.table(file = path, header = TRUE, sep = ";", encoding = "UTF-8")
+
+rounds <- NA
+while(is.na(rounds)){
+    rounds <- readline("How many rounds would you like to do?: ")
+    rounds <- as.numeric(rounds)
+    if(is.na(rounds)){print("Please choose a number")}
+}
+print(paste0("We will do ",rounds," rounds."))
+
+if(reverse == FALSE){
+    for(i in 1:rounds){
+        selected <- sample(x = 1:nrow(data), size = 1)
+        answer <- readline(paste0(data[selected, 1], "\nYour answer: "))
+        print(paste0("The correct answer is ", data[selected, 2]))
+    }
+}
+
+if(reverse == TRUE){
+    for(i in 1:rounds){
+        selected <- sample(x = 1:nrow(data), size = 1)
+        answer <- readline(paste0(data[selected, 2], "\nYour answer: "))
+        print(paste0("The correct answer is ", data[selected, 1]))
+    }
+}
