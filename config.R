62073952d394535621bd808b28a0f26a07465720
Eivind Kjeka Broen <eivind.kb@hotmail.com>
2023-06-22 09:06:19 +0200

    new folder in mega

--- config.R
+++ config.R
@@ -1,6 +1,6 @@
 # config file for flashcards
 
 setwd("../")
-path <- paste0(getwd(),"/mega/flashcards_data/spanish.csv")
+path <- paste0(getwd(),"/mega/data/flashcards/spanish.csv")
 reverse = FALSE
 
